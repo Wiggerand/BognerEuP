b7e5d304c4a7eed2ffb61916aa0f8e1bbe1adbdf
awiggers <awiggers@live.de>
2016-11-29 12:57:38 +0100

    RailsUpDate

--- rails/Projekte/rails_app/app/helpers/events_helper.rb
+++ rails/Projekte/rails_app/app/helpers/events_helper.rb
@@ -1,2 +1,15 @@
 module EventsHelper
+
+	def format_price(event)
+
+		if event.free?
+
+			content_tag(:strong, 'FREE')
+			#{}"<strong>Free</strong>".html_safe
+		else
+			number_to_currency event.price
+		end
+		
+	end
+
 end
--- rails/Projekte/rails_app/app/models/event.rb
+++ rails/Projekte/rails_app/app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ApplicationRecord
+
+	def free? 
+
+		price.blank? || price.zero?
+			
+		
+		end
 end
