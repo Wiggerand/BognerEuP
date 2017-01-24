4fd8a0a03f1a49303835076657f7939f3650a3a2
awiggers <awiggers@live.de>
2017-01-24 12:55:26 +0100

    UserLogin

--- rails/Projekte/rails_app/app/controllers/application_controller.rb
+++ rails/Projekte/rails_app/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def current_user
+  	User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  before_action :set_locale
+
+  def set_locale
+  	I18n.locale = I18n.default_locale
+  end
 end
--- rails/Projekte/rails_app/app/controllers/sessions_controller.rb
+++ rails/Projekte/rails_app/app/controllers/sessions_controller.rb
@@ -5,6 +5,18 @@ class SessionsController < ApplicationController
 	end
 	def create
 		user = User.find_by(email: params[:email])
-		
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			flash[:notice] = "Welcome back #{user.name.capitalize}!" 
+			redirect_to root_path
+		else
+			flash.now[:alert] = "Falsche Zugangsdaten, überprüfe bitte deine Eingaben. Eingegebene email Adresse #{user.email}"
+			render :new
+		end	
+	end
+
+	def destroy
+		session[:user_id] = nil
+		redirect_to root_path, notice: "Ausgeloggt. Bye, bye Tiger!"
 	end
 end
