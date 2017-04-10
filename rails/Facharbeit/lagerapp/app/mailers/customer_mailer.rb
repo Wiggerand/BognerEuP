bb3dc75af2e389a4ccc24646e6174bf1e6d66723
awiggers <awiggers@live.de>
2017-04-10 10:12:45 +0200

    EmailTop

--- rails/Facharbeit/lagerapp/app/mailers/customer_mailer.rb
+++ rails/Facharbeit/lagerapp/app/mailers/customer_mailer.rb
@@ -1,11 +1,11 @@
-class CustomerMailer < ActionMailer::Base #ApplicationMailer
-	default from: 'Ihre freundliche Autowerkstatt autowerkstatt@web.de'
+class CustomerMailer < ApplicationMailer #ActionMailer::Base
+	default from: 'autowerkstatt@web.de'
   layout 'mailer'
 
 	def sendmail(customer)
 		@customer = Customer.first
 
-		mail(to: @customer.email, subject: "Alter, mach hinneeeeeeeeeeeeeee")#@customer.email.where(privacypolicy: true), subject: "Welcome to Autowerkstatt")
+		mail(to: @customer.email, subject: "Die neue Saison steht vor der Tür!")#@customer.email.where(privacypolicy: true), subject: "Welcome to Autowerkstatt")
 		
 	end
 
--- rails/Facharbeit/lagerapp/config/environments/development.rb
+++ rails/Facharbeit/lagerapp/config/environments/development.rb
@@ -4,19 +4,21 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  # Don't care if the mailer can't send.
-
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.raise_delivery_errors = true
+  
+  #config.action_mailer.delivery_method = :letter_opener
+  
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.web.de',
-    port:                 587,
-    user_name:            'autowerkstatt@web.de',
-    password:             'Autowerkstatt2017',
-    authentication:       'plain',
-    enable_starttls_auto: true
+    #:address =>              'smtp.gmail.com',
+    :address =>              'smtp.web.de',
+    :port =>                 587,
+    #:user_name =>            'human.resource.fabian.knoop@gmail.com',
+    :user_name =>            'autowerkstatt@web.de',
+    #:password =>             '%G16Admin',
+    :password =>             'Autowerkstatt2017',
+    :authentication =>       'plain',
+    :enable_starttls_auto => true
 
   }
 
@@ -42,8 +44,9 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  # Don't care if the mailer can't send.
 
-  
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
